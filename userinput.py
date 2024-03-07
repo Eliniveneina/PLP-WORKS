3db4477e899b19fcb689f5c500f133a1d2f3dc1a
Eliniveneina <160838459+Eliniveneina@users.noreply.github.com>
2024-03-07 21:27:52 +0300

    Initial Commit

---
+++ userinput.py
@@ -0,0 +1,6 @@
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+location = input("Where are you from: ")
+
+print(f"\nHello {name}, You are {age} years old and you come from '{location}'")
+
